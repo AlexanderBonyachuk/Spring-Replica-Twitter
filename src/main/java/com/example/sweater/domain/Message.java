bfc20ddaef9bde4aaa7b67b57e54670459e298d1
Sasha40404 <70843523+Sasha40404@users.noreply.github.com>
2022-08-29 20:14:30 +0400

    Add view your messages

--- src/main/java/com/example/sweater/domain/Message.java
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -8,7 +8,7 @@ import javax.validation.constraints.NotBlank;
 @Entity
 public class Message {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
 
     @NotBlank(message = "Please fill the message")
@@ -20,27 +20,36 @@ public class Message {
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id")
     private User author;
+
     private String filename;
 
     public Message() {
     }
 
     public Message(String text, String tag, User user) {
+        this.author = user;
         this.text = text;
         this.tag = tag;
-        this.author = user;
     }
 
     public String getAuthorName() {
         return author != null ? author.getUsername() : "<none>";
     }
 
-    public String getText() {
-        return text;
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
     }
 
     public void setText(String text) {
         this.text = text;
+    }
+
+    public String getText() {
+        return text;
     }
 
     public Long getId() {
@@ -57,14 +66,6 @@ public class Message {
 
     public void setTag(String tag) {
         this.tag = tag;
-    }
-
-    public User getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(User author) {
-        this.author = author;
     }
 
     public String getFilename() {
